5e20ca62c44b08f19dfa376cb874530489194825
TerabyteX <Don_Knuth@163.com>
2014-09-03 13:36:53 +0800

    first commit

---
+++ MD.EventMQ.Samples/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MD.EventMQ.Samples
+{
+    class Program
+    {
+        private static readonly EventMQ<string> mq = new EventMQ<string>();
+
+        static void Main(string[] args)
+        {
+            mq.OnMessage += ConsumerA;
+            mq.OnMessage += (obj, e) =>
+            {
+                Console.WriteLine("调用ConsumerB的线程ID为：{0}", Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine("ConsumerB: " + obj);
+            };
+            mq.OnMessage += ConsumerC;
+            mq.OnMessage += ConsumerD;
+            mq.OnMessage -= ConsumerD;
+
+            //for (var i = 0; i < 1000; i++)
+            //{
+            mq.Publish("test1");
+            mq.Publish("test2");
+            mq.Publish("test3");
+            mq.Publish(new List<string> { "7", "8", "9" });
+            //}
+
+            Console.Read();
+        }
+
+        private static void ConsumerA(object sender, EventArgs e)
+        {
+            Console.WriteLine("调用ConsumerA的线程ID为：{0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("ConsumerA: " + sender);
+        }
+
+        private static void ConsumerC(object sender, EventArgs e)
+        {
+            Console.WriteLine("调用ConsumerC的线程ID为：{0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("ConsumerC: " + sender);
+        }
+
+        private static void ConsumerD(object sender, EventArgs e)
+        {
+            Console.WriteLine("调用ConsumerD的线程ID为：{0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("ConsumerD: " + sender);
+        }
+    }
+}
---
+++ MD.EventMQ/EventMQ.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MD.EventMQ
+{
+    public class EventMQ<T>
+    {
+        //public event EventHandler<MqEventArgs> OnMessage;
+        public event EventHandler OnMessage
+        {
+            add { _event.Add(value); }
+            remove { _event.Remove(value); }
+        }
+        private WeakEvent<EventHandler> _event = new WeakEvent<EventHandler>();
+
+        public void Publish(T msg)
+        {
+            OnPublish(msg, MqEventArgs.Empty);
+        }
+
+        public void Publish(IEnumerable<T> msgs)
+        {
+            foreach (var msg in msgs)
+            {
+                OnPublish(msg, MqEventArgs.Empty);
+            }
+        }
+
+        public void OnPublish(T msg, MqEventArgs e)
+        {
+            //System.Threading.ThreadPool.QueueUserWorkItem((obj) => OnMessage(msg, e));
+            System.Threading.ThreadPool.QueueUserWorkItem((obj) => _event.Raise(msg, e));
+        }
+    }
+
+    public class MqEventArgs : EventArgs
+    {
+        public new static readonly MqEventArgs Empty;
+    }
+}
